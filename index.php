b1ae65077b01b8b8db7643f24db267021516ae31
Anatema <anatema153@gmail.com>
2015-09-21 18:04:37 +0300

    commit 1

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+<?php
+$message = "Привет, я - скрипт PHP!";
+echo $message;
+?>
+
+</body>
+</html>
